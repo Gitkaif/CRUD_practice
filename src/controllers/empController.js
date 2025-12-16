e86d3a389d0e0b6f412d47cd6217385c2f88c627
Gitkaif <kaifbscit101@gmail.com>
2025-12-16 10:07:57 +0530

    add logic

--- src/controllers/empController.js
+++ src/controllers/empController.js
@@ -1,10 +1,32 @@
 const Emp = require('../models/empModel');
 
+
+// for only single record insert
+
+// exports.addEmp = async (req, res) => {   
+//     const newEmp = new Emp(req.body);
+//     const ressult = await newEmp.save();
+//     res.status(201).json({ message: "New user added..." }, ressult);
+// };
+
+// for both single and multiple records insert
+
 exports.addEmp = async (req, res) => {
-    const newEmp = new Emp(req.body);
-    const ressult = await newEmp.save();
-    res.status(201).json({ message: "New user added..." }, ressult);
+    try {
+        let result;
+    if (Array.isArray(req.body)) {
+        result = await Emp.insertMany(req.body);
+    }
+    else {
+        const newEmp = new Emp(req.body);
+        result = await newEmp.save();
+    }
+    res.status(201).json({ message: "New user added..." }, result);
+    } catch (error) {
+        res.status(500).json({message:error.message});
+    }
 };
+
 
 exports.showEmp = async (req, res) => {
     try {
@@ -21,10 +43,10 @@ exports.showEmp = async (req, res) => {
 
 exports.deleteEmp = async (req, res) => {
     try {
-        const result = await Emp.findByIdAndDelete(req.params._id);
+        const result = await Emp.findByIdAndDelete(req.params.id);
         res.status(200).json(result);
     } catch (error) {
         // console.log(error);
-        res.status(500).json({message:"emp not deleted"})
+        res.status(500).json({ message: "emp not deleted" })
     }
 }
--- src/models/empModel.js
+++ src/models/empModel.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose');
 
 const empSchema = new mongoose.Schema({
-    id:{
-        type:Number,
-        required:true
-    },
     name:{
         type:String,
         required:true
